5d8338fd39fdc69526511aa677efcfa3fb23b77a
Artem Serooky <adrenalin497@gmail.com>
2023-08-08 17:16:19 +0300

    Added project structure and some dependencies

--- app/src/main/java/com/example/translater/MainActivity.kt
+++ app/src/main/java/com/example/translater/MainActivity.kt
@@ -3,13 +3,9 @@ package com.example.translater
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.translater.ui.theme.TranslaterTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,31 +13,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TranslaterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "translater_main_screen"
                 ) {
-                    Greeting("World")
+                    composable("translater_main_screen") {
+
+                    }
+                    composable("translater_favorites_screen") {
+
+                    }
+                    composable("translater_quiz_screen") {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        style = MaterialTheme.typography.titleMedium
-    )
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    TranslaterTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/translater/ui/theme/Type.kt
+++ app/src/main/java/com/example/translater/ui/theme/Type.kt
@@ -9,101 +9,118 @@ import androidx.compose.ui.unit.sp
 import com.example.translater.R
 
 val Roboto = FontFamily(
+    listOf(
     Font(R.font.roboto_regular, FontWeight.Normal),
     Font(R.font.roboto_bold, FontWeight.Bold),
     Font(R.font.roboto_black, FontWeight.Black),
     Font(R.font.roboto_light, FontWeight.Light),
     Font(R.font.roboto_thin, FontWeight.Thin),
     Font(R.font.roboto_medium, FontWeight.Medium)
+    )
 )
 
 val Typography = Typography(
     displayLarge = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 57.sp,
         lineHeight = 64.sp,
-        letterSpacing = 0.sp
+        letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp
     ),
     displaySmall = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 1.sp
     ),
     labelLarge = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     bodyLarge = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
         fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
